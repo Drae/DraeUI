a43e59eebc77ff49bad5c3daeeb613711fd458ca
Drae <paul@starstreak.net>
2014-10-15 00:08:42 +0100

    Introduce some fixes from latest kuinameplates

--- modules/nameplates/auras.lua
+++ modules/nameplates/auras.lua
@@ -12,6 +12,7 @@ local PLA = PL:NewModule("Auras", "AceEvent-3.0")
 
 --]]
 local _G = _G
+local UnitExists, UnitGUID = UnitExists,UnitGUID
 local GetTime, floor, ceil = GetTime, math.floor, math.ceil
 
 --[[
@@ -39,7 +40,7 @@ end
 local WhitelistChanged = function()
 	-- inform listeners of whitelist update
 	for _, listener in ipairs(listeners) do
-		if (listener[1])[listener[2]] then
+		if ((listener[1])[listener[2]]) then
 			(listener[1])[listener[2]]()
 		end
 	end
@@ -58,11 +59,11 @@ local GetDefaultSpells = function(class, onlyClass)
 	local list = {}
 
 	-- return a copy of the list rather than a reference
-	for spellid,_ in pairs(PL.db.auras.whitelist[class]) do
+	for spellid, _ in pairs(PL.db.auras.whitelist[class]) do
 		list[spellid] = true
 	end
 
-	if not onlyClass then
+	if (not onlyClass) then
 		AppendGlobalSpells(list)
 	end
 
@@ -72,29 +73,7 @@ end
 local GetImportantSpells = function(class)
 	-- get spell list and merge with KuiSpellListCustom if it is set
 	local list = GetDefaultSpells(class)
---[[
-	if KuiSpellListCustom then
-		for _,group in pairs({class,'GlobalSelf'}) do
-			if KuiSpellListCustom.Ignore and
-			   KuiSpellListCustom.Ignore[group]
-			then
-				-- remove ignored spells
-				for spellid,_ in pairs(KuiSpellListCustom.Ignore[group]) do
-					list[spellid] = nil
-				end
-			end
 
-			if KuiSpellListCustom.Classes and
-			   KuiSpellListCustom.Classes[group]
-			then
-				-- merge custom added spells
-				for spellid,_ in pairs(KuiSpellListCustom.Classes[group]) do
-					list[spellid] = true
-				end
-			end
-		end
-	end
-	]]
 	return list
 end
 
@@ -102,14 +81,14 @@ local ArrangeButtons = function(self)
 	local pv, pc
 	self.visible = 0
 
-	for k,b in ipairs(self.buttons) do
-		if b:IsShown() then
+	for k, b in ipairs(self.buttons) do
+		if (b:IsShown()) then
 			self.visible = self.visible + 1
 
 			b:ClearAllPoints()
 
-			if pv then
-				if (self.visible-1) % (self.frame.trivial and 3 or 5) == 0 then
+			if (pv) then
+				if ((self.visible - 1) % (self.frame.trivial and 3 or 5) == 0) then
 					-- start of row
 					b:SetPoint("BOTTOMLEFT", pc, "TOPLEFT", 0, 1)
 					pc = b
@@ -127,7 +106,7 @@ local ArrangeButtons = function(self)
 		end
 	end
 
-	if self.visible == 0 then
+	if (self.visible == 0) then
 		self:Hide()
 	else
 		self:Show()
@@ -149,10 +128,10 @@ do
 	end
 
 	DoPulsateAura = function(button)
-		if button.fading or not button.doPulsate then return end
+		if (button.fading or not button.doPulsate) then return end
 		button.fading = true
 
-		if button.faded then
+		if (button.faded) then
 			PL.frameFade(button, {
 				startAlpha = .5,
 				timeToFade = .5,
@@ -180,23 +159,21 @@ end
 local OnAuraUpdate = function(self, elapsed)
 	self.elapsed = self.elapsed - elapsed
 
-	if self.elapsed <= 0 then
+	if (self.elapsed <= 0) then
 		local timeLeft = self.expirationTime - GetTime()
 
-		if PL.db.display.pulsate then
-			if self.doPulsate and timeLeft > FADE_THRESHOLD then
+		if (PL.db.display.pulsate) then
+			if (self.doPulsate and timeLeft > FADE_THRESHOLD) then
 				-- reset pulsating status if the time is extended
 				StopPulsatingAura(self)
-			elseif not self.doPulsate and timeLeft <= FADE_THRESHOLD then
+			elseif (not self.doPulsate and timeLeft <= FADE_THRESHOLD) then
 				-- make the aura pulsate
 				self.doPulsate = true
 				DoPulsateAura(self)
 			end
 		end
 
-		if PL.db.display.decimal and
-		   timeLeft <= 2 and timeLeft > 0
-		then
+		if (PL.db.display.decimal and timeLeft <= 2 and timeLeft > 0) then
 			-- faster updates in the last two seconds
 			self.elapsed = .05
 		else
@@ -213,7 +190,7 @@ end
 local OnAuraHide = function(self)
 	local parent = self:GetParent()
 
-	if parent.spellIds[self.spellId] == self then
+	if (parent.spellIds[self.spellId] == self) then
 		parent.spellIds[self.spellId] = nil
 	end
 
@@ -228,20 +205,20 @@ end
 local GetAuraButton = function(self, spellId, icon, count, duration, expirationTime)
 	local button
 
-	if self.spellIds[spellId] then
+	if (self.spellIds[spellId]) then
 		-- use this spell's current button...
 		button = self.spellIds[spellId]
-	elseif self.visible ~= #self.buttons then
+	elseif (self.visible ~= #self.buttons) then
 		-- .. or reuse a hidden button...
 		for k, b in pairs(self.buttons) do
-			if not b:IsShown() then
+			if (not b:IsShown()) then
 				button = b
 				break
 			end
 		end
 	end
 
-	if not button then
+	if (not button) then
 		button = CreateFrame("Frame", nil, self)
 		button:Hide()
 
@@ -249,11 +226,11 @@ local GetAuraButton = function(self, spellId, icon, count, duration, expirationT
 		border:SetPoint("TOPLEFT", button, -6, 6)
 		border:SetPoint("BOTTOMRIGHT", button, 6, -6)
 		border:SetFrameStrata("BACKGROUND")
-		border:SetBackdrop {
+		border:SetBackdrop({
 			edgeFile = "Interface\\AddOns\\draeUI\\media\\textures\\glowtex",
 			tile = false,
 			edgeSize = 6
-		}
+		})
 		border:SetBackdropBorderColor(0, 0, 0, 0.5)
 		button.border = border
 
@@ -289,7 +266,7 @@ local GetAuraButton = function(self, spellId, icon, count, duration, expirationT
 	button:SetSize(PL.db.auraiconsize, PL.db.auraiconsize)
 	button.icon:SetTexture(icon)
 
-	if count > 1 and not self.frame.trivial then
+	if (count > 1 and not self.frame.trivial) then
 		button.count:SetText(count)
 		button.count:Show()
 	else
@@ -340,7 +317,7 @@ end
 
 function PLA:Show(msg, frame)
 	-- set vertical position of the container frame
-	if frame.trivial then
+	if (frame.trivial) then
 		frame.auras:SetPoint("BOTTOMLEFT", frame.hp, "TOPLEFT",
 			0, 8)
 	else
@@ -350,25 +327,37 @@ function PLA:Show(msg, frame)
 end
 
 function PLA:Hide(msg, frame)
-	if frame.auras then
+	if (frame.auras) then
 		frame.auras:Hide()
 	end
 end
 
 -------------------------------------------------------------- event handlers --
 function PLA:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
+	-- used to hide expired auras on previously known frames
+	-- and to detect aura updates on the mouseover, if it exists
+	-- (since UNIT_AURA doesn"t fire for mouseover)
 	local castTime, event, _, guid, name, _, _, targetGUID, targetName = ...
-	if not guid then return end
-	if not auraEvents[event] then return end
-	if guid ~= UnitGUID("player") then return end
+
+	if (not guid) then return end
+	if (guid ~= UnitGUID("player")) then return end
+
+	if (UnitGUID("mouseover") == targetGUID) then
+		-- update the mouseover's auras
+		self:UNIT_AURA("UNIT_AURA","mouseover")
+	end
+
+	-- only listen for removals from now
+	if (not auraEvents[event]) then return end
 
 	-- fetch the subject"s nameplate
 	local f = PL:GetNameplate(targetGUID, targetName)
-	if not f or not f.auras then return end
+
+	if (not f or not f.auras) then return end
 
 	local spId = select(12, ...)
 
-	if f.auras.spellIds[spId] then
+	if (f.auras.spellIds[spId]) then
 		f.auras.spellIds[spId]:Hide()
 	end
 end
@@ -384,11 +373,12 @@ end
 function PLA:UNIT_AURA(event, unit)
 	-- select the unit"s nameplate
 	local frame = PL:GetNameplate(UnitGUID(unit), nil)
-	if not frame or not frame.auras then return end
-	if frame.trivial and not PL.db.showtrivial then return end
+
+	if (not frame or not frame.auras) then return end
+	if (frame.trivial and not PL.db.showtrivial) then return end
 
 	local filter = "PLAYER "
-	if UnitIsFriend(unit, "player") then
+	if (UnitIsFriend(unit, "player")) then
 		filter = filter.."HELPFUL"
 	else
 		filter = filter.."HARMFUL"
@@ -409,9 +399,9 @@ function PLA:UNIT_AURA(event, unit)
 		end
 	end
 
-	for _,button in pairs(frame.auras.buttons) do
+	for _, button in pairs(frame.auras.buttons) do
 		-- hide buttons that weren't used this update
-		if not button.used then
+		if (not button.used) then
 			button:Hide()
 		end
 
@@ -442,7 +432,7 @@ function PLA:OnEnable()
 
 	local _, frame
 	for _, frame in pairs(PL.frameList) do
-		if not frame.auras then
+		if (not frame.auras) then
 			self:Create(nil, frame.draePlate)
 		end
 	end
