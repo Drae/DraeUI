30517d0005d45778e09a540c73fc998ab9abbc35
Drae <paul@starstreak.net>
2015-02-27 14:46:40 +0000

    Remove unneeded code

--- media/sharedmedia.lua
+++ media/sharedmedia.lua
@@ -3,10 +3,5 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 LSM:Register("font",		"Bignoodle",		"Interface\\AddOns\\draeUI\\media\\fonts\\Bignoodle.ttf")
 LSM:Register("font",		"Liberation Sans",	"Interface\\AddOns\\draeUI\\media\\fonts\\LiberationSans.ttf")
-LSM:Register("font",		"PT_Sans_Narrow",	"Interface\\AddOns\\draeUI\\media\\fonts\\PT_Sans_Narrow.ttf")
-LSM:Register("font",		"Invisible",		"Interface\\AddOns\\draeUI\\media\\fonts\\Invisible.ttf")
 
 LSM:Register("statusbar",	"Striped",			"Interface\\AddOns\\draeUI\\media\\statusbars\\Striped")
-
-LSM:Register("sound",		"Warning",			"Interface\\AddOns\\draeUI\\media\\sounds\\warning.mp3")
-LSM:Register("sound",		"Whisper Alert",	"Interface\\AddOns\\draeUI\\media\\sounds\\whisper.mp3")
