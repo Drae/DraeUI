8e1f50b1e350c4027047d72863679b266776cd6f
Paul Owen <drae@users.noreply.github.com>
2016-08-28 01:31:38 +0100

    Unused/wanted textures, add new font

--- media/sharedmedia.lua
+++ media/sharedmedia.lua
@@ -4,5 +4,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 LSM:Register("font",		"Bignoodle",		"Interface\\AddOns\\draeUI\\media\\fonts\\Bignoodle.ttf")
 LSM:Register("font",		"Liberation Sans",	"Interface\\AddOns\\draeUI\\media\\fonts\\LiberationSans.ttf")
 LSM:Register("font",		"Proza",			"Interface\\AddOns\\draeUI\\media\\fonts\\Proza.ttf")
+LSM:Register("font",		"Oblik Bold",		"Interface\\AddOns\\draeUI\\media\\fonts\\oblikbold.ttf")
 
 LSM:Register("statusbar",	"Striped",			"Interface\\AddOns\\draeUI\\media\\statusbars\\Striped")
+LSM:Register("statusbar",	"Guild Wars",		"Interface\\AddOns\\draeUI\\media\\statusbars\\statusbarsfill")
