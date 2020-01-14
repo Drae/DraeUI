82dc5dded321ddbe93c0e989cd67656ebe1458c2
Paul Owen <drae@users.noreply.github.com>
2020-01-14 15:57:58 +0000

    Range of random updates

--- config/config.defaults.lua
+++ config/config.defaults.lua
@@ -13,62 +13,55 @@ local T, C, G, P, U, _ = select(2, ...):UnPack()
 		into that new file. You don't need to copy the entire
 		section just the variable/s you want to change.
 --]]
-
 C["general"] = {
--- Textures
+	-- Textures
 	statusbar = "Striped",
 	font = "Liberation Sans",
 	fontFancy = "Proza",
-
 	fontsize0 = 16,
 	fontsize1 = 13,
 	fontsize2 = 12,
 	fontsize3 = 10,
-	fontsize4 = 9,
+	fontsize4 = 9
 }
 
 -- Unit Frame settings
 C["frames"] = {
-	numFormatLong		= false,
-
+	numFormatLong = false,
 	-- Display or hide frames
-	showBoss			= true, 	-- Boss frames
-	showArena			= true,
-
+	showBoss = true, -- Boss frames
+	showArena = true,
 	-- Player and Target are positioned relative to center of screen,
 	-- all other frames are positioned relative to those
-	playerXoffset		= -450,
-	playerYoffset		= -120,
-	targetXoffset		= 450,
-	targetYoffset		= -120,
-	totXoffset			= 25, 	-- Relative to right of target
-	totYoffset			= 0,
-	focusXoffset		= 6,  	-- Relative to left of target
-	focusYoffset		= 75,
-	focusTargetXoffset	= 25, 	-- Relative to right of focus target
-	focusTargetYoffset	= 0,
-	petXoffset			= -6, --62, 	-- Relative to left of player
-	petYoffset			= 30, ---100,
-	petTargetXoffset	= 6,  	-- Relative to right of pet
-	petTargetYoffset	= -10,
-	bossXoffset			= 0,	-- Relative to left of target
-	bossYoffset			= 300,
-	arenaXoffset		= 0, 	-- Relative to left of player
-	arenaYoffset		= 140,
-
-	largeScale 			= 1.0,
-	mediumScale 		= 1.0,
-	smallScale 			= 1.0,
-
+	playerXoffset = -450,
+	playerYoffset = -120,
+	targetXoffset = 450,
+	targetYoffset = -120,
+	totXoffset = 25, -- Relative to right of target
+	totYoffset = 0,
+	focusXoffset = 6, -- Relative to left of target
+	focusYoffset = 75,
+	focusTargetXoffset = 25, -- Relative to right of focus target
+	focusTargetYoffset = 0,
+	petXoffset = -6, --62, 	-- Relative to left of player
+	petYoffset = 30, ---100,
+	petTargetXoffset = 6, -- Relative to right of pet
+	petTargetYoffset = -10,
+	bossXoffset = 0, -- Relative to left of target
+	bossYoffset = 300,
+	arenaXoffset = 0, -- Relative to left of player
+	arenaYoffset = 140,
+	largeScale = 1.0,
+	mediumScale = 1.0,
+	smallScale = 1.0,
 	-- Dimension of frames, large applies to player/target, small everything else
 	-- don't change these, change the scale
-	largeWidth 			= 200,
-	largeHeight 		= 20,
-	mediumWidth			= 140,
-	mediumHeight		= 20,
-	smallWidth 			= 90,
-	smallHeight 		= 20,
-
+	largeWidth = 200,
+	largeHeight = 20,
+	mediumWidth = 140,
+	mediumHeight = 20,
+	smallWidth = 90,
+	smallHeight = 20,
 	-- Aura settings
 	auras = {
 		-- Large are debuffs on players, buffs on targets, Sml are buffs on player,
@@ -77,70 +70,57 @@ C["frames"] = {
 		auraLrg = 26,
 		auraSml = 24,
 		auraTny = 22,
-
 		auraMag = 1.8, -- Multiplier for the magnified view of auras
-
-		maxPlayerBuff		 	= 10,
-		maxPlayerDebuff 		= 4,
-		maxPetBuff		 		= 4,
-		maxPetDebuff 			= 4,
-		maxTargetBuff			= 4,
-		maxTargetDebuff			= 10,
-		maxFocusBuff			= 4,
-		maxFocusDebuff			= 4,
-		maxFocusTargetBuff		= 10,
-		maxOtherBuff			= 2,
-		maxOtherDebuff			= 2,
-		maxBossBuff				= 1,
-		maxArenaBuff			= 4,
-
+		maxPlayerBuff = 10,
+		maxPlayerDebuff = 4,
+		maxPetBuff = 4,
+		maxPetDebuff = 4,
+		maxTargetBuff = 4,
+		maxTargetDebuff = 10,
+		maxFocusBuff = 4,
+		maxFocusDebuff = 4,
+		maxFocusTargetBuff = 10,
+		maxOtherBuff = 2,
+		maxOtherDebuff = 2,
+		maxBossBuff = 1,
+		maxArenaBuff = 4,
 		buffs_per_row = {
-			["player"]	= 5,
-			["target"]	= 2,
-			["focus"]	= 2,
-			["focustarget"]	= 4,
-			["boss"]	= 2,
-			["other"]	= 2, -- focus, focus target, pet, etc.
+			["player"] = 5,
+			["target"] = 2,
+			["focus"] = 2,
+			["focustarget"] = 4,
+			["boss"] = 2,
+			["other"] = 2 -- focus, focus target, pet, etc.
 		},
-
 		debuffs_per_row = {
-			["player"]	= 2,
-			["target"]	= 5,
-			["focus"]	= 2,
-			["other"]	= 2,
+			["player"] = 2,
+			["target"] = 5,
+			["focus"] = 2,
+			["other"] = 2
 		},
-
-		showBuffsOnMe 			= true, -- Short term buffs on myself or my pet
-		showDebuffsOnMe 		= true, -- Debuffs on myself or pet
-		showBuffsOnFriends		= true, -- Buffs on friends (excluding 0 duration auras)
-		showDebuffsOnFriends	= true,
-		showBuffsOnEnemies		= true,
-		showDebuffsOnEnemies	= true,
-
-		showStealableBuffs		= true,
-
+		showBuffsOnMe = true, -- Short term buffs on myself or my pet
+		showDebuffsOnMe = true, -- Debuffs on myself or pet
+		showBuffsOnFriends = true, -- Buffs on friends (excluding 0 duration auras)
+		showDebuffsOnFriends = true,
+		showBuffsOnEnemies = true,
+		showDebuffsOnEnemies = true,
+		showStealableBuffs = true,
 		-- These auras are never displayed regardless of any other settings
 		blacklistAuraFilter = {
 			["Chill of the Throne"] = true,
-			["Strength of Wrynn"] 	= true,
+			["Strength of Wrynn"] = true
 		},
-
 		filterType = "WHITELIST", -- dictates which filter we"ll use
-
 		-- If debuff filtering is enabled only the debuffs in the following list will appear on targets
 		whiteListFilter = {
-			["DEBUFF"] = {
-			},
-			["BUFF"] = {
-			}
+			["DEBUFF"] = {},
+			["BUFF"] = {}
 		},
 		blackListFilter = {
-			["DEBUFF"] = {
-			},
-			["BUFF"] = {
-			}
-		},
-	},
+			["DEBUFF"] = {},
+			["BUFF"] = {}
+		}
+	}
 }
 
 C["raidframes"] = {
@@ -148,78 +128,74 @@ C["raidframes"] = {
 	width = 60,
 	height = 35,
 	gridLayout = "HORIZONTAL", -- groups are arranged horizontally - one above (or below) the other,
-							   -- VERTICAL would have groups appear to the right (or left) of each other
+	-- VERTICAL would have groups appear to the right (or left) of each other
 	gridGroupsAnchor = "BOTTOMLEFT", -- This is the anchor point for each group - groups will grow from this point
 	padding = 2, -- Distance between frames - the highlight border is 3px, so keep it >3
 	showPets = true, -- Pets will be shown as seperate units, vehicles will appear as pets if enabled
-
 	colorSmooth = true,
 	colorPet = false,
 	colorCharmed = true,
-
 	indicatorLrg = 8,
 	indicatorSml = 5,
-
 	-- X, Y position of frames - the 1, 2, 3, etc. tables
 	-- Equates to the total number of groups in the raid (not each group!). If you do not
 	-- specify a position for a total number of groups the position of the last highest
 	-- will be used
 	position = {
-		[1] = { "BOTTOM", UIParent, "CENTER", 0, -415 }, --275
-		[6] = { "BOTTOM", UIParent, "CENTER", -780, -100 },
+		[1] = {"BOTTOM", UIParent, "CENTER", 0, -415}, --275
+		[6] = {"BOTTOM", UIParent, "CENTER", -850, -130}
 	},
-
 	-- Button parameters
 	raidnamelength = 6,
 	showRaidHealthPct = false, -- Show health as a "remaining percentage" rather than an "absolute deficit"
-	showOnlyDispellable = false, -- true to only show dispellable unknown debuffs
+	showOnlyDispellable = false -- true to only show dispellable unknown debuffs
 }
 
 -- Player, target and focus castbar
 C["castbar"] = {
 	player = {
-		width	 = 200,
-		height	 = 8,
-		xOffset  = -6,
-		yOffset  = 40,
-		anchor 	 = "DraePlayer",
+		width = 200,
+		height = 8,
+		xOffset = -6,
+		yOffset = 40,
+		anchor = "DraePlayer",
 		anchorat = "RIGHT",
-		anchorto = "RIGHT",
+		anchorto = "RIGHT"
 	},
 	target = {
-		width	 = 200,
-		height	 = 8,
-		xOffset  = 7,
-		yOffset  = 40,
-		anchor 	 = "DraeTarget",
+		width = 200,
+		height = 8,
+		xOffset = 7,
+		yOffset = 40,
+		anchor = "DraeTarget",
 		anchorat = "LEFT",
-		anchorto = "LEFT",
+		anchorto = "LEFT"
 	},
 	focus = {
-		width	 = 150,
-		height	 = 6,
-		xOffset  = 3,
-		yOffset  = 54,
-		anchor 	 = "DraeFocus",
+		width = 150,
+		height = 6,
+		xOffset = 3,
+		yOffset = 54,
+		anchor = "DraeFocus",
 		anchorat = "LEFT",
-		anchorto = "LEFT",
+		anchorto = "LEFT"
 	},
 	boss = {
-		width	 = 150,
-		height	 = 6,
-		xOffset  = -12,
-		yOffset  = 0,
+		width = 150,
+		height = 6,
+		xOffset = -12,
+		yOffset = 0,
 		anchorat = "RIGHT",
-		anchorto = "LEFT",
+		anchorto = "LEFT"
 	},
 	arena = {
-		width	 = 150,
-		height	 = 6,
-		xOffset  = 0,
-		yOffset  = 12,
+		width = 150,
+		height = 6,
+		xOffset = 0,
+		yOffset = 12,
 		anchorat = "LEFT",
-		anchorto = "RIGHT",
-	},
+		anchorto = "RIGHT"
+	}
 }
 
 C["infobar"] = {
@@ -235,22 +211,21 @@ C["infobar"] = {
 G["chat"] = {
 	sticky = true,
 	shortChannels = true,
-
 	timeStampFormat = "%H:%M ",
 	scrollDownInterval = 15,
-	throttleInterval = 45,
+	throttleInterval = 45
 }
 
 -- Minimap
 G["minimap"] = {
-	buttons	= {
-		["MiniMapTracking"] 			= { angle = 25 },  -- The tracking button/menu
-		["MiniMapMailFrame"] 			= { angle = 310 }, -- New mail alert
-		["QueueStatusMinimapButton"] 	= { angle = 212 }, -- Dungeon Finder
-		["GameTimeFrame"] 				= { angle = 50 },  -- The Calendar
-		["MiniMapInstanceDifficulty"] 	= { angle = 126 }, -- Instance difficulty
-		["GuildInstanceDifficulty"] 	= { angle = 126 }, -- As above when in guild group
-		["MiniMapChallengeMode"] 		= { angle = 126 }, -- As above when in doing challenge modes
-		["TimeManagerClockButton"] 		= { anchorat = "BOTTOM", anchorto = "BOTTOM", posx = 0, posy = -11 }, -- The clock
-	},
+	buttons = {
+		["MiniMapTracking"] = {angle = 25}, -- The tracking button/menu
+		["MiniMapMailFrame"] = {angle = 310}, -- New mail alert
+		["QueueStatusMinimapButton"] = {angle = 212}, -- Dungeon Finder
+		["GameTimeFrame"] = {angle = 50}, -- The Calendar
+		["MiniMapInstanceDifficulty"] = {angle = 126}, -- Instance difficulty
+		["GuildInstanceDifficulty"] = {angle = 126}, -- As above when in guild group
+		["MiniMapChallengeMode"] = {angle = 126}, -- As above when in doing challenge modes
+		["TimeManagerClockButton"] = {anchorat = "BOTTOM", anchorto = "BOTTOM", posx = 0, posy = -11} -- The clock
+	}
 }
--- config/raiddebuffs/battleforazeroth.lua
+++ config/raiddebuffs/battleforazeroth.lua
@@ -6,86 +6,61 @@ UF["raiddebuffs"]["instances"]["Uldir"] = function()
 	-- enable, spell, priority, secondary, pulse, flash
 
 	-- Taloc
-	UF:AddRaidDebuff(true, 270290, 5) -- Blood Storms 
-	UF:AddRaidDebuff(true, 271222, 5) -- Plasma Discharge 
-	UF:AddRaidDebuff(true, 271296, 5) -- Cudgel of Gore 
+	UF:AddRaidDebuff(true, 270290, 5) -- Blood Storms
+	UF:AddRaidDebuff(true, 271222, 5) -- Plasma Discharge
+	UF:AddRaidDebuff(true, 271296, 5) -- Cudgel of Gore
 
 	-- MOTHER
-	UF:AddRaidDebuff(true, 267787, 5) -- Sanitizing Strike 
-	UF:AddRaidDebuff(true, 268198, 5) -- Clinging Corruption 
-	UF:AddRaidDebuff(true, 267821, 5) -- Defense Grids 
+	UF:AddRaidDebuff(true, 267787, 5) -- Sanitizing Strike
+	UF:AddRaidDebuff(true, 268198, 5) -- Clinging Corruption
+	UF:AddRaidDebuff(true, 267821, 5) -- Defense Grids
 
 	-- Fetid Devourer
-	UF:AddRaidDebuff(true, 262313, 5) -- Malodorous Miasma 
-	UF:AddRaidDebuff(true, 262314, 5) -- Putrid Paroxysm 
+	UF:AddRaidDebuff(true, 262313, 5) -- Malodorous Miasma
+	UF:AddRaidDebuff(true, 262314, 5) -- Putrid Paroxysm
 
 	-- Zek'voz, herald of N'zoth
-	UF:AddRaidDebuff(true, 265264, 5) -- Void Lash 
-	UF:AddRaidDebuff(true, 265646, 5) -- Will of the Corruptor 
-	UF:AddRaidDebuff(true, 265360, 5) -- Roiling Deceit 
-	UF:AddRaidDebuff(true, 267334, 5) -- Orb of Corruption 
-	UF:AddRaidDebuff(true, 265662, 5) -- Corruptor's Pact 
-	UF:AddRaidDebuff(true, 265451, 5) -- Surging Darkness 
+	UF:AddRaidDebuff(true, 265264, 5) -- Void Lash
+	UF:AddRaidDebuff(true, 265646, 5) -- Will of the Corruptor
+	UF:AddRaidDebuff(true, 265360, 5) -- Roiling Deceit
+	UF:AddRaidDebuff(true, 267334, 5) -- Orb of Corruption
+	UF:AddRaidDebuff(true, 265662, 5) -- Corruptor's Pact
+	UF:AddRaidDebuff(true, 265451, 5) -- Surging Darkness
 
 	-- Vectis
-	UF:AddRaidDebuff(true, 265143, 6) -- Omega Vector 
+	UF:AddRaidDebuff(true, 265143, 6) -- Omega Vector
 	UF:AddRaidDebuff(true, 265127, 8, true) -- Lingering Infection
-	UF:AddRaidDebuff(true, 265178, 5) -- Evolving Affliction 
-	UF:AddRaidDebuff(true, 265206, 6) -- Immunosuppression 
-	UF:AddRaidDebuff(true, 266948, 5) -- Plague Bomb 
-	UF:AddRaidDebuff(true, 265212, 6, nil, nil, true) -- Gestate 
+	UF:AddRaidDebuff(true, 265178, 5) -- Evolving Affliction
+	UF:AddRaidDebuff(true, 265206, 6) -- Immunosuppression
+	UF:AddRaidDebuff(true, 266948, 5) -- Plague Bomb
+	UF:AddRaidDebuff(true, 265212, 6, nil, nil, true) -- Gestate
 
 	-- Zul, Reborn
-	UF:AddRaidDebuff(true, 274195, 5) -- Corrupted Blood 
-	UF:AddRaidDebuff(true, 274358, 5) -- Rupturing Blood 
-	UF:AddRaidDebuff(true, 274363, 5) -- Ruptured Blood 
-	UF:AddRaidDebuff(true, 273365, 5) -- Dark Revelation 
-	UF:AddRaidDebuff(true, 274271, 6) -- Deathwish 
+	UF:AddRaidDebuff(true, 274195, 5) -- Corrupted Blood
+	UF:AddRaidDebuff(true, 274358, 5) -- Rupturing Blood
+	UF:AddRaidDebuff(true, 274363, 5) -- Ruptured Blood
+	UF:AddRaidDebuff(true, 273365, 5) -- Dark Revelation
+	UF:AddRaidDebuff(true, 274271, 6) -- Deathwish
 
 	-- Mythrax the Unraveler
 	UF:AddRaidDebuff(true, 272336, 5, true) -- Annihilation (stacks)
 	UF:AddRaidDebuff(true, 273282, 6) -- Essence Shear (Tank)
-	UF:AddRaidDebuff(true, 272536, 6) -- Imminent Ruin 
+	UF:AddRaidDebuff(true, 272536, 6) -- Imminent Ruin
 	UF:AddRaidDebuff(true, 272407, 6) -- Oblivion Sphere (charmed)
 
 	-- G'huun
-	UF:AddRaidDebuff(true, 263372, 5) -- Power Matrices 
-	UF:AddRaidDebuff(true, 263503, 5) -- Reorigination Blasts 
-	UF:AddRaidDebuff(true, 267409, 5) -- Dark Bargain 
-	UF:AddRaidDebuff(true, 270447, 5) -- Growing Corruption 
-	UF:AddRaidDebuff(true, 263236, 5) -- Blood Feast 
-	UF:AddRaidDebuff(true, 263334, 5) -- Putrid Blood 
-	UF:AddRaidDebuff(true, 267700, 5) -- Gaze of G'huun 
-
+	UF:AddRaidDebuff(true, 263372, 5) -- Power Matrices
+	UF:AddRaidDebuff(true, 263503, 5) -- Reorigination Blasts
+	UF:AddRaidDebuff(true, 267409, 5) -- Dark Bargain
+	UF:AddRaidDebuff(true, 270447, 5) -- Growing Corruption
+	UF:AddRaidDebuff(true, 263236, 5) -- Blood Feast
+	UF:AddRaidDebuff(true, 263334, 5) -- Putrid Blood
+	UF:AddRaidDebuff(true, 267700, 5) -- Gaze of G'huun
 end
 
 -- Battle for Dazar'alor
 UF["raiddebuffs"]["instances"]["Battle of Dazar'alor"] = function()
-	UF:AddRaidDebuff(true, 289384, 5) --Savage Swipe
-	UF:AddRaidDebuff(true, 287437, 5) --Frozen Beam
-	UF:AddRaidDebuff(true, 287294, 5) --Blizzard
-	UF:AddRaidDebuff(true, 286740, 5) --Light's Fury
-	UF:AddRaidDebuff(true, 285250, 5) --Sundering Slam
-	UF:AddRaidDebuff(true, 289645, 5) --Polymorph
-	UF:AddRaidDebuff(true, 289578, 5) --Lit Torch
-	UF:AddRaidDebuff(true, 287325, 5) --Comet Storm
-	UF:AddRaidDebuff(true, 289772, 5) --Impale
-	UF:AddRaidDebuff(true, 285292, 5) --Frozen Tides
-	UF:AddRaidDebuff(true, 285280, 5) --Searing Waves
-	UF:AddRaidDebuff(true, 287147, 5) --Dread Reaping
-	UF:AddRaidDebuff(true, 284573, 5) --Tailwinds
-	UF:AddRaidDebuff(true, 253562, 5) --Wildfire
 	UF:AddRaidDebuff(true, 289917, 5, true) --Bwonsamdi's Pact
-	UF:AddRaidDebuff(true, 289418, 5) --Predator's Gaze
-	UF:AddRaidDebuff(true, 287295, 5) --Chilled
-	UF:AddRaidDebuff(true, 289937, 5) --Thundering Slam
-	UF:AddRaidDebuff(true, 289514, 5) --Molten Ground
-	UF:AddRaidDebuff(true, 289512, 5) --Molten Punch
-	UF:AddRaidDebuff(true, 289373, 5) --Lacerating Pounce
-	UF:AddRaidDebuff(true, 287456, 5) --Frost Nova
-	UF:AddRaidDebuff(true, 287556, 5) --Soul Barrage
-	UF:AddRaidDebuff(true, 289484, 5) --Burning Bite
-	UF:AddRaidDebuff(true, 289918, 5) --Bwonsamdi's Bargain
 
 	-- Champions
 	UF:AddRaidDebuff(true, 283617, 5) --Wave of Light
@@ -132,13 +107,13 @@ UF["raiddebuffs"]["instances"]["Battle of Dazar'alor"] = function()
 	UF:AddRaidDebuff(true, 285195, 5) -- Deathly withering
 	UF:AddRaidDebuff(true, 288449, 6) -- Deaths Door
 	UF:AddRaidDebuff(true, 286742, 8) -- Necrotic Smash (tank)
-	
+
 	-- Mekka'torque
 	UF:AddRaidDebuff(true, 286646, 8) -- Gigavolt charge
 	UF:AddRaidDebuff(true, 287891, 8) -- Sheep Shrapnel
 	UF:AddRaidDebuff(true, 289699, 8, true) -- Electroshock Amplification (tanks)
 	UF:AddRaidDebuff(true, 284168, 8, true) -- Shrunk!
-	
+
 	-- Stonewall
 	UF:AddRaidDebuff(true, 284106, 8) -- Crackling lightning - Sister Katherine
 	UF:AddRaidDebuff(true, 284360, 8) -- Sea Storm
@@ -150,7 +125,7 @@ UF["raiddebuffs"]["instances"]["Battle of Dazar'alor"] = function()
 	UF:AddRaidDebuff(true, 287993, 5, true) --Chilling Touch
 	UF:AddRaidDebuff(true, 288394, 5, true) --Warmth
 	UF:AddRaidDebuff(true, 285253, 5) --Ice Shard
-	UF:AddRaidDebuff(true, 288169, 5) --Howling Winds	
+	UF:AddRaidDebuff(true, 288169, 5) --Howling Winds
 	UF:AddRaidDebuff(true, 287490, 5) --Frozen Solid
 	UF:AddRaidDebuff(true, 288297, 5) --Arctic Ground
 	UF:AddRaidDebuff(true, 287365, 5) --Searing Pitch
@@ -162,4 +137,23 @@ UF["raiddebuffs"]["instances"]["Battle of Dazar'alor"] = function()
 	UF:AddRaidDebuff(true, 288038, 5) --Marked Target
 	UF:AddRaidDebuff(true, 288434, 5) --Hand of Frost
 	UF:AddRaidDebuff(true, 287199, 5) --Ring of Ice
+end
+
+-- The Eternal Palace
+UF["raiddebuffs"]["instances"]["The Eternal Palace"] = function()
+	-- Za'qul
+	UF:AddRaidDebuff(true, 292963, 5) -- Dread
+	UF:AddRaidDebuff(true, 292971, 7, true) -- Hysteria
+	UF:AddRaidDebuff(true, 300133, 7) -- Snapped
+	UF:AddRaidDebuff(true, 293509, 7) -- Manifest Nightmares
+
+	-- Queen Azshara
+	UF:AddRaidDebuff(true, 298014, 5, true, true) -- Cold Blast
+	UF:AddRaidDebuff(true, 303799, 8) -- Beckon
+	UF:AddRaidDebuff(true, 303816, 9) -- Beckon
+	UF:AddRaidDebuff(true, 302999, 7) -- Arcane Vulnerability
+	UF:AddRaidDebuff(true, 303657, 5, nil, true) -- Arcane Burst - DON'T DISPELL
+	UF:AddRaidDebuff(true, 300492, 6) -- Static Shock
+	UF:AddRaidDebuff(true, 300743, 5, true) -- Void Touched
+	UF:AddRaidDebuff(true, 298569, 4, true) -- Drained Soul
 end
