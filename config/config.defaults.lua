2b1021ea83b66e4553b32476984092f5fb51c045
Paul Owen <drae@users.noreply.github.com>
2021-02-19 00:14:38 +0000

    Update default config

--- config/config.defaults.lua
+++ config/config.defaults.lua
@@ -15,7 +15,7 @@ local T, C, G, P, U, _ = select(2, ...):UnPack()
 --]]
 C["general"] = {
 	-- Textures
-	statusbar = "Striped",
+	statusbar = "striped",
 	font = "Proza",
 	fontsize0 = 16,
 	fontsize1 = 13,
@@ -29,12 +29,12 @@ C["frames"] = {
 	numFormatLong = false,
 	-- Display or hide frames
 	showBoss = true, -- Boss frames
-	showArena = true,
+	showArena = false,
 	-- Player and Target are positioned relative to center of screen,
 	-- all other frames are positioned relative to those
-	playerXoffset = -350,
+	playerXoffset = -360,
 	playerYoffset = -250,
-	targetXoffset = 350,
+	targetXoffset = 360,
 	targetYoffset = -250,
 	totXoffset = 30, -- Relative to right of target
 	totYoffset = 0,
@@ -46,14 +46,14 @@ C["frames"] = {
 	petYoffset = -120, ---100,
 	bossXoffset = 0, -- Relative to left of target
 	bossYoffset = 300,
-	arenaXoffset = 0, -- Relative to left of player
-	arenaYoffset = 140,
+	arenaXoffset = 0, -- Relative to left of target
+	arenaYoffset = 300,
 	largeScale = 1.0,
 	mediumScale = 1.0,
 	smallScale = 1.0,
 	-- Dimension of frames, large applies to player/target, small everything else
 	-- don't change these, change the scale
-	largeWidth = 250,
+	largeWidth = 260,
 	largeHeight = 20,
 	mediumWidth = 180,
 	mediumHeight = 20,
@@ -63,27 +63,26 @@ C["frames"] = {
 	auras = {
 		-- Large are debuffs on players, buffs on targets, Sml are buffs on player,
 		-- debuffs on target and tiny are buffs/debuffs on other units
-		auraHge = 24,
-		auraLrg = 22,
-		auraSml = 20,
+		auraHge = 28,
+		auraLrg = 25,
+		auraSml = 22,
 		auraTny = 18,
 		auraMag = 1.8, -- Multiplier for the magnified view of auras
-		maxPlayerBuff = 6,
-		maxPlayerDebuff = 3,
+		maxPlayerBuff = 8,
+		maxPlayerDebuff = 6,
 		maxPetBuff = 3,
-		maxPetDebuff = 2,
-		maxTargetBuff = 2,
+		maxPetDebuff = 3,
+		maxTargetBuff = 8,
 		maxTargetDebuff = 6,
 		maxFocusBuff = 3,
 		maxFocusDebuff = 3,
 		maxFocusTargetBuff = 3,
 		maxOtherBuff = 3,
 		maxOtherDebuff = 3,
-		maxBossBuff = 1,
-		maxArenaBuff = 3,
+		maxBossBuff = 2,
 		buffs_per_row = {
-			["player"] = 6,
-			["target"] = 3,
+			["player"] = 4,
+			["target"] = 4,
 			["focus"] = 3,
 			["focustarget"] = 3,
 			["boss"] = 3,
@@ -91,7 +90,7 @@ C["frames"] = {
 		},
 		debuffs_per_row = {
 			["player"] = 3,
-			["target"] = 6,
+			["target"] = 3,
 			["focus"] = 3,
 			["other"] = 3
 		},
@@ -123,30 +122,52 @@ C["frames"] = {
 
 C["raidframes"] = {
 	-- General frame parameters
-	width = 45,
-	height = 45,
+	width = 50,
+	height = 50,
+	petHeight = 25,
 	gridLayout = "HORIZONTAL", -- groups are arranged horizontally - one above (or below) the other,
 	-- VERTICAL would have groups appear to the right (or left) of each other
 	gridGroupsAnchor = "BOTTOMRIGHT", -- This is the anchor point for each group - groups will grow from this point
-	padding = 2, -- Distance between frames - the highlight border is 3px, so keep it >3
+	padding = 10, -- Distance between frames - the highlight border is 3px, so keep it >3
+	petOffset = 16, -- Offset from player frames for pet/vehicle frames
 	showPets = true, -- Pets will be shown as seperate units, vehicles will appear as pets if enabled
-	colorSmooth = true,
+	powerHeight = 4,	-- Height of power bar (shown only for healers)
+	colorSmooth = false,
 	colorPet = false,
 	colorCharmed = true,
 	indicatorLrg = 5,
-	indicatorSml = 3,
 	-- X, Y position of frames - the 1, 2, 3, etc. tables
 	-- Equates to the total number of groups in the raid (not each group!). If you do not
 	-- specify a position for a total number of groups the position of the last highest
 	-- will be used
 	position = {
-		[1] = {"BOTTOMRIGHT", UIParent, "CENTER", -223, -170}, --275
-		[6] = {"BOTTOMRIGHT", UIParent, "CENTER", -223, -170}
+		[1] = {"BOTTOMRIGHT", UIParent, "CENTER", -228, -150}, --275
+		[6] = {"BOTTOMLEFT", UIParent, "LEFT", 50, -150}
+	},
+	scale = {
+		[1] = 1,
+		[6] = 0.8
 	},
 	-- Button parameters
 	raidnamelength = 4,
 	showRaidHealthPct = false, -- Show health as a "remaining percentage" rather than an "absolute deficit"
-	showOnlyDispellable = false -- true to only show dispellable unknown debuffs
+	showOnlyDispellable = true, -- true to only show dispellable unknown debuffs
+	-- These auras are never displayed regardless of any other settings
+	blacklistAuraFilter = {
+		["Chill of the Throne"] = true,
+		["Strength of Wrynn"] = true,
+		["Grasping Tendrils"] = true
+	},
+	filterType = "WHITELIST", -- dictates which filter we"ll use
+	-- If debuff filtering is enabled only the debuffs in the following list will appear on targets
+	whiteListFilter = {
+		["DEBUFF"] = {},
+		["BUFF"] = {}
+	},
+	blackListFilter = {
+		["DEBUFF"] = {},
+		["BUFF"] = {}
+	}
 }
 
 -- Player, target and focus castbar
@@ -191,8 +212,8 @@ C["castbar"] = {
 		height = 7,
 		xOffset = 2,
 		yOffset = 10,
-		anchorat = "LEFT",
-		anchorto = "RIGHT"
+		anchorat = "RIGHT",
+		anchorto = "LEFT"
 	}
 }
 
