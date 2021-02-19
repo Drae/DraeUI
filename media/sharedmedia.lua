786825e5d225a883aa1fbf9701ad35c8faf54702
Paul Owen <drae@users.noreply.github.com>
2021-02-19 00:20:56 +0000

    New media additions Remove Lib Sans font - using just Proza now

--- media/sharedmedia.lua
+++ media/sharedmedia.lua
@@ -2,7 +2,6 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
 LSM:Register("font",		"Bignoodle",		"Interface\\AddOns\\draeUI\\media\\fonts\\Bignoodle.ttf")
---LSM:Register("font",		"Liberation Sans",	"Interface\\AddOns\\draeUI\\media\\fonts\\LiberationSans.ttf")
 LSM:Register("font",		"Proza",			"Interface\\AddOns\\draeUI\\media\\fonts\\Proza.ttf")
 
-LSM:Register("statusbar",	"Striped",			"Interface\\AddOns\\draeUI\\media\\statusbars\\Striped")
+LSM:Register("statusbar",	"striped",			"Interface\\AddOns\\draeUI\\media\\statusbars\\striped")
