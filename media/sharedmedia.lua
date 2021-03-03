c4a058795827481b3a5f469e1baff9f175165720
Paul Owen <drae@users.noreply.github.com>
2021-03-03 23:36:44 +0000

    Update fonts

--- media/sharedmedia.lua
+++ media/sharedmedia.lua
@@ -1,7 +1,10 @@
 -- Register with SharedMedia
 local LSM = LibStub("LibSharedMedia-3.0")
 
-LSM:Register("font",		"Bignoodle",		"Interface\\AddOns\\draeUI\\media\\fonts\\Bignoodle.ttf")
-LSM:Register("font",		"Proza",			"Interface\\AddOns\\draeUI\\media\\fonts\\Proza.ttf")
+LSM:Register("font",		"Bignoodle",		"Interface\\AddOns\\draeUI\\media\\fonts\\bignoodle.ttf")
+LSM:Register("font",		"Proza",			"Interface\\AddOns\\draeUI\\media\\fonts\\prozaregular-regular.ttf")
+LSM:Register("font",		"Alegreya",			"Interface\\AddOns\\draeUI\\media\\fonts\\alegreya-medium.ttf")
+LSM:Register("font",		"Vollkorn",			"Interface\\AddOns\\draeUI\\media\\fonts\\vollkorn-medium.ttf")
+LSM:Register("font",		"LiberationSans",	"Interface\\AddOns\\draeUI\\media\\fonts\\liberationsans.ttf")
 
 LSM:Register("statusbar",	"striped",			"Interface\\AddOns\\draeUI\\media\\statusbars\\striped")
