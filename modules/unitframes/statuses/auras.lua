24cf503caf37d0653a43a14cbbdf4942c162d790
Paul Owen <drae@users.noreply.github.com>
2021-03-15 00:04:09 +0000

    Fix show of indicators that are "mine" only Also pre-initialize various tables

--- modules/unitframes/statuses/auras.lua
+++ modules/unitframes/statuses/auras.lua
@@ -9,7 +9,7 @@ local Status = UF:NewModule("StatusAuras")
 
 -- Localise a bunch of functions
 local UnitAura, UnitIsDeadOrGhost = UnitAura, UnitIsDeadOrGhost
-local pairs = pairs
+local pairs, wipe = pairs, wipe
 
 --
 local aura_buffs = {}
@@ -19,7 +19,7 @@ local aura_debuffs = {}
 
 --]]
 local UpdateIndicatorList = function()
-	local auras = DraeUI.dbClass["auras"]
+	local auras = DraeUI.dbClass.auras
 	if (not auras) then return end
 
 	wipe(aura_buffs)
@@ -28,11 +28,21 @@ local UpdateIndicatorList = function()
 	for status, aura in pairs(auras) do
 		if (not aura.disable) then
 			if (aura.buff) then
-				aura_buffs[aura.buff] = aura
-				aura_buffs[aura.buff]["status"] = status
+				aura_buffs[aura.buff] = {
+					status = status,
+					color = aura.color or { 1.0, 1.0, 1.0 },
+					pulse = aura.pulse or nil,
+					flash = aura.flash or nil,
+					mine = aura.mine or true
+				}
 			elseif (aura.debuff) then
-				aura_debuffs[aura.debuff] = aura
-				aura_debuffs[aura.debuff]["status"] = status
+				aura_debuffs[aura.debuff] = {
+					status = status,
+					color = aura.color or { 1.0, 1.0, 1.0 },
+					pulse = aura.pulse or nil,
+					flash = aura.flash or nil,
+					mine = aura.mine or true
+				}
 			end
 		end
 	end
@@ -70,14 +80,14 @@ do
 						player_buffs_seen[name] = true
 
 						-- status, color, texture, text, value, maxValue, start, duration, stack, notMine, pulse, flash
-						self:GainedStatus(aura.status, aura.color, texture, aura.text, nil, nil, start, duration, stack, nil, aura.pulse, aura.flash)
-					else
+						self:GainedStatus(aura.status, aura.color, texture, nil, nil, nil, start, duration, stack, nil, aura.pulse, aura.flash)
+					elseif (not aura.mine) then
 						buffs_seen[name] = true
 
 						local notMine = caster and caster ~= "player" and true or false
 
 						-- status, color, texture, text, value, maxValue, start, duration, stack, notMine, pulse, flash
-						self:GainedStatus(aura.status, aura.color, texture, aura.text, nil, nil, start, duration, stack, notMine, aura.pulse, aura.flash)
+						self:GainedStatus(aura.status, aura.color, texture, nil, nil, nil, start, duration, stack, notMine, aura.pulse, aura.flash)
 					end
 				end
 
@@ -99,7 +109,7 @@ do
 					debuffs_seen[name] = true
 
 					-- status, color, texture, text, value, maxValue, start, duration, stack, notMine, pulse, flash
-					self:GainedStatus(aura.status, aura.color, texture, aura.text, nil, nil, start, duration, stack, nil, aura.pulse, aura.flash)
+					self:GainedStatus(aura.status, aura.color, texture, nil, nil, nil, start, duration, stack, nil, aura.pulse, aura.flash)
 				end
 
 				index = index + 1
