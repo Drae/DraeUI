8c99571d7e8df0e0bf3f19f4297f3b783f90bfd3
Paul Owen <drae@users.noreply.github.com>
2022-07-09 09:21:09 +0100

    Some Shadowlands raid debuffs

--- config/raiddebuffs/shadowlands.lua
+++ config/raiddebuffs/shadowlands.lua
@@ -103,3 +103,112 @@ UF["raiddebuffs"]["instances"]["Castle Nathria"] = function()
 	UF:AddRaidDebuff(true, 328276, 5) -- March of the Penitent
 	UF:AddRaidDebuff(true, 327992, 6) -- Desolation
 end
+
+-- Sepulcher
+UF["raiddebuffs"]["instances"]["Sepulcher of the First Ones"] = function()
+	-- enable, spell, priority, secondary, pulse, flash
+
+	-- Vigilant Guardian
+	UF:AddRaidDebuff(true, 360458, 7, true) -- Unstable Core
+	UF:AddRaidDebuff(true, 360414, 5, true) -- Pneumatic Impact (tank)
+	UF:AddRaidDebuff(true, 367571, 6) -- Sear (heroic)
+	UF:AddRaidDebuff(true, 364447, 6) -- Dissonance (tank)
+	UF:AddRaidDebuff(true, 360202, 5) -- Spike of Creation
+	UF:AddRaidDebuff(true, 364881, 5) -- Matter Dissolution
+
+	-- Skolex, the Insatiable Ravener
+	UF:AddRaidDebuff(true, 359778, 5) -- Ephemera Dust
+	UF:AddRaidDebuff(true, 360098, 5) -- Warp Sickness
+	UF:AddRaidDebuff(true, 366070, 5) -- Volatile Residue
+	UF:AddRaidDebuff(true, 364522, 7) -- Devouring Blood
+	UF:AddRaidDebuff(true, 359976, 5) -- Riftmaw
+
+	-- Artificer Xy'mox
+	UF:AddRaidDebuff(true, 363413, 5) -- Genesis Rings
+	UF:AddRaidDebuff(true, 365752, 5) -- Ancient Exhaust
+	UF:AddRaidDebuff(true, 365801, 5) -- Relic Collapse
+	UF:AddRaidDebuff(true, 363114, 5) -- Genesis Supernova
+	UF:AddRaidDebuff(true, 365682, 5) -- Massive Blast
+	UF:AddRaidDebuff(true, 364030, 5) -- Debilitating Ray
+
+	-- Dausegne, the Fallen Oracle
+	UF:AddRaidDebuff(true, 361966, 5) -- Infused Strikes
+	UF:AddRaidDebuff(true, 361751, 5) -- Disintegration Halo
+	UF:AddRaidDebuff(true, 364289, 4) -- Disintegration Halo
+
+	-- Prototype Pantheon
+	UF:AddRaidDebuff(true, 360687, 5, nil, true) -- Runecarvers Deathtouch
+	UF:AddRaidDebuff(true, 361067, 5, true) -- Bastion's Ward (negates Runecarvers)
+	UF:AddRaidDebuff(true, 360259, 5) -- Gloom Bolt
+	UF:AddRaidDebuff(true, 365041, 5) -- Windswept  Wings
+	UF:AddRaidDebuff(true, 362352, 5) -- Pinned
+	UF:AddRaidDebuff(true, 361784, 4) -- Touch of the Night
+	UF:AddRaidDebuff(true, 362383, 5) -- Anima Bolt
+	UF:AddRaidDebuff(true, 361689, 5) -- Wracking Pain
+	UF:AddRaidDebuff(true, 361608, 5) -- Burden of Sin
+	UF:AddRaidDebuff(true, 364867, 5) -- Sinful Projection
+
+	-- Lihuvim, Principal Architect
+	UF:AddRaidDebuff(true, 360159, 5) -- Unstable Mote
+	UF:AddRaidDebuff(true, 363681, 5) -- Deconstructing Blast
+	UF:AddRaidDebuff(true, 368024, 5) -- Kinetic Resonance
+	UF:AddRaidDebuff(true, 368025, 5) -- Sundering Resonance
+	UF:AddRaidDebuff(true, 364092, 5) -- Degenerate
+	UF:AddRaidDebuff(true, 366012, 5) -- Terminal Mote
+	UF:AddRaidDebuff(true, 362447, 4) -- Instability
+
+	-- Halondrus the Reclaimer
+	UF:AddRaidDebuff(true, 368961, 5) -- Charge Exposure
+	UF:AddRaidDebuff(true, 365294, 5) -- Charge Exposure
+	UF:AddRaidDebuff(true, 360114, 5) -- Ephemeral Fissure
+	UF:AddRaidDebuff(true, 361597, 5) -- Ephemeral Eruption
+	UF:AddRaidDebuff(true, 368529, 5) -- Eternity Overdrive
+
+	-- Anduin Wrynn
+	UF:AddRaidDebuff(true, 368913, 4) -- Force of Will
+	UF:AddRaidDebuff(true, 362055, 5, true) -- Lost Soul
+	UF:AddRaidDebuff(true, 365445, 5) -- Scarred Soul
+	UF:AddRaidDebuff(true, 364031, 6) -- Gloom
+	UF:AddRaidDebuff(true, 365293, 7, nil, true) -- Befouled Barrier
+	UF:AddRaidDebuff(true, 366849, 6) -- Domination Word Pain
+	UF:AddRaidDebuff(true, 363024, 6) -- Necrotic Detonation
+	UF:AddRaidDebuff(true, 363020, 5) -- Necrotic Claws
+	UF:AddRaidDebuff(true, 363028, 5) -- Unraveling Frenzy
+	UF:AddRaidDebuff(true, 368428, 5) -- Purging Light
+
+	-- Lords of Dread
+	UF:AddRaidDebuff(true, 360284, 6) -- Anguishing Strike (tank - stacks)
+	UF:AddRaidDebuff(true, 359963, 6) -- Opened Veins (tank)
+	UF:AddRaidDebuff(true, 360241, 7) -- Unsettling Dreams (sleep - dispell)
+	UF:AddRaidDebuff(true, 360148, 5, true) -- Bursting Dread (clears Cloud of Carrion)
+	UF:AddRaidDebuff(true, 360008, 7, true) -- Cloud of Carrion
+	UF:AddRaidDebuff(true, 364985, 8) -- Biting Wounds (from above - stacks)
+	UF:AddRaidDebuff(true, 360300, 5) -- Swarm of Decay
+	UF:AddRaidDebuff(true, 360304, 5) -- Swarm of Darkness
+
+	-- Rygelon
+	UF:AddRaidDebuff(true, 361548, 6, nil, true) -- Dark Eclipse
+	UF:AddRaidDebuff(true, 361462, 5, true) -- Collapsing Quasar Field
+	UF:AddRaidDebuff(true, 362172, 5) -- Corrupted Wound (tank)
+	UF:AddRaidDebuff(true, 362081, 6) -- Cosmic Ejection (heroic)
+	UF:AddRaidDebuff(true, 364386, 5) -- Gravitational Collapse
+
+	-- The Jailer
+	UF:AddRaidDebuff(true, 366381, 5) -- Arcane Vulnerability
+	UF:AddRaidDebuff(true, 366703, 5) -- Azerite Radiation
+	UF:AddRaidDebuff(true, 362075, 6) -- Domination
+	UF:AddRaidDebuff(true, 362631, 5) -- Chains of Oppression
+	UF:AddRaidDebuff(true, 363893, 5) -- Martyrdom
+	UF:AddRaidDebuff(true, 363886, 5) -- Imprisonment
+	UF:AddRaidDebuff(true, 362194, 5) -- Suffering
+	UF:AddRaidDebuff(true, 362192, 4) -- Relentless Misery
+	UF:AddRaidDebuff(true, 366665, 5) -- Unholy Eruption
+	UF:AddRaidDebuff(true, 359868, 5) -- Shattering Blast
+	UF:AddRaidDebuff(true, 366777, 5) -- Consumed Azerite
+	UF:AddRaidDebuff(true, 360425, 5) -- Unholy Ground
+	UF:AddRaidDebuff(true, 366285, 5) -- Rune of Compulsion
+	UF:AddRaidDebuff(true, 365153, 5) -- Dominating Will
+	UF:AddRaidDebuff(true, 365219, 5) -- Chains of Anguish
+	UF:AddRaidDebuff(true, 365385, 6) -- Cry of Loathing
+	UF:AddRaidDebuff(true, 365174, 5) -- Defile
+end
